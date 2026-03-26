e02fab714c97e5393fd3dade8c41fdde2475daba
Prince Patra <princepatra9951@gmail.com>
2026-03-26 17:30:23 +0530

    Updated landing.js

--- landing.js
+++ landing.js
@@ -8,4 +8,3 @@ loginBtn.addEventListener("click", function () {
 signupBtn.addEventListener("click", function () {
         window.location.href = "signup.html";
 });
-// This code adds event listeners to the login and signup buttons on the landing page. When the login button is clicked, it redirects the user to the "signin.html" page, and when the signup button is clicked, it redirects the user to the "signup.html" page.
