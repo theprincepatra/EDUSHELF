f625f33ca4d20f7343e514677b3623c19481ea31
Prince Patra <princepatra9951@gmail.com>
2026-05-09 10:26:01 +0530

    added sidebar and poitioned the center div for the content

--- codes/public/js/dashboard.js
+++ codes/public/js/dashboard.js
@@ -1,9 +1,13 @@
-function openMenu() {
-    document.getElementById("sidebar").style.left = "0";
-    document.getElementById("overlay").style.display = "block";
-}
-
-function closeMenu() {
-    document.getElementById("sidebar").style.left = "-260px";
-    document.getElementById("overlay").style.display = "none";
+function toggleMenu() {
+    const sidebar = document.getElementById("sidebar");
+    const main = document.querySelector(".main");
+    
+    if (sidebar.style.left === "0px") {
+        sidebar.style.left = "-260px";
+        main.style.marginLeft = "132px";
+    }
+    else {
+        sidebar.style.left = "0px";
+        main.style.marginLeft = "200px";
+    }
 }
