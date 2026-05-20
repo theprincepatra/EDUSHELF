a8f0fa1d5ec82ca731c93dbd52e1179705c15864
Prince Patra <princepatra9951@gmail.com>
2026-05-20 09:53:41 +0530

    added new fields in the schema

--- codes/models/user.js
+++ codes/models/user.js
@@ -2,11 +2,24 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/siginupDB');
 
 const userSchema = new mongoose.Schema({
+
+    // initial fields
     userId: { type: Number, unique: true },
     name: String,
     username: { type: String, unique: true },
     email: { type: String, unique: true },
     password: String,
+
+    // extra fields
+    profilepicture: { type: String, default: '/images/default-profile.png' },
+    phonenumber: String,
+    hostel: String,
+    department: String,
+    yearofstudy: Number,
+    dob: Date,
+
+    // profile status
+    isProfileComplete: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('user', userSchema);
