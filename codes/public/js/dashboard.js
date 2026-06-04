ebbfad55dd7abc7584d2a90a2c84f3c21b6c33cd
Prince Patra <princepatra9951@gmail.com>
2026-06-04 11:53:13 +0530

    added responsive alyout to the desciption page

--- codes/public/js/dashboard.js
+++ codes/public/js/dashboard.js
@@ -1,20 +1,21 @@
 function toggleMenu() {
     const sidebar = document.getElementById("sidebar");
     const main = document.querySelector(".main");
-    
+
     if (sidebar.style.left === "0px") {
         sidebar.style.left = "-260px";
-        main.style.marginLeft = "132px";
+        main.classList.remove("shift");
     }
     else {
         sidebar.style.left = "0px";
-        main.style.marginLeft = "200px";
+        main.classList.add("shift");
     }
 }
 
 function showSection(sectionId){
     const sections = document.querySelectorAll(".section");
-    sections.forEach(function(section){
+
+    sections.forEach(section => {
         section.classList.remove("active");
     });
 
