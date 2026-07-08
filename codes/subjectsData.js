e718e8f06c98a6433d88ff3060bd6a03f4fbcec8
Prince Patra <princepatra9951@gmail.com>
2026-07-08 23:28:42 +0530

    added folder structure

--- codes/subjectsData.js
+++ codes/subjectsData.js
@@ -5,31 +5,37 @@ const subjectsData = {
             {
                 code:"CSE1002",
                 name:"Descrete Mathematics",
+                slug: "Descrete-Mathematics",
                 credits:4
             },
             {
                 code:"CSE1001",
                 name:"Introduction to Computer Programming",
+                slug: "Introduction-to-Computer-Programming",
                 credits:4
             },
             {
                 code:"MTH1101",
                 name:"Calculus A",
+                slug:"Calculus-A",
                 credits:4
             },
             {
                 code:"PHY1001",
                 name:"University Physics: Mechanics",
+                slug:"University-Physics-Mechanics",
                 credits:4
             },
             {
                 code:"GEN1140",
                 name:"Digital Fabrication Techniques",
+                slug:"Digital-Fabrication-Techniques",
                 credits:1
             },
             {
                 code:"HSS1972",
                 name:"Universal Human Values",
+                slug:"Universal-Human-Values",
                 credits:1
             }
         ],
@@ -37,31 +43,37 @@ const subjectsData = {
             {
                 code:"CSE1004",
                 name:"Introduction to Graph Theory",
+                slug:"Introduction-to-Graph-Theory",
                 credits:4
             },
             {
                 code:"CSE2001",
                 name:"Data Structures and Algorithms",
+                slug:"Data-Structures-and-Algorithms",
                 credits:4
             },
             {
                 code:"CS2101",
                 name:"Calculus B",
+                slug:"Calculus-B",
                 credits:4
             },
             {
                 code:"PHY2001",
                 name:"University Physics: Electricity and Magnetism",
+                slug:"University-Physics-Electricity-and-Magnetism",
                 credits:4
             },
             {
                 code:"GEN1166",
                 name:"Introductory Containerization for Developers",
+                slug:"Introductory-Containerization-for-Developers",
                 credits:1
             },
             {
                 code:"HSS1972",
                 name:"Introductory Technical Writing",
+                slug:"Introductory-Technical-Writing",
                 credits:3
             }
         ],
@@ -69,31 +81,37 @@ const subjectsData = {
             {
                 code:"CSE2141",
                 name:"Computer Science Workshop 1",
+                slug:"Computer-Science-Workshop-1",
                 credits:4
             },
             {
                 code:"EET1121",
                 name:"Digital Logic Design",
+                slug:"Digital-Logic-Design",
                 credits:4
             },
             {
                 code:"CSE2631",
-                name:"Algorithm Analysis and Design 1",
+                name:"Algorithim Analysis and Design 1",
+                slug:"Algorithim-Analysis-and-Design-1",
                 credits:4
             },
             {
                 code:"MTH2002",
                 name:"Probability and Statistics",
+                slug:"Probability-and-Statistics",
                 credits:4
             },
             {
                 code:"HSS2023",
                 name:"Introduction to Macroeconomics Economics",
+                slug:"Introduction-to-Macroeconomics-Economics",
                 credits:3
             },
             {
                 code:"CHM2041",
                 name:"Introduction to Environmental Studies",
+                slug:"Introduction-to-Environmental-Studies",
                 credits:2
             }
         ],
@@ -101,31 +119,37 @@ const subjectsData = {
             {
                 code:"CSE3141",
                 name:"Computer Science Workshop 2",
+                slug:"Computer-Science-Workshop-2",
                 credits:4
             },
             {
                 code:"EET2211",
                 name:"Commputer Organisation and Architecture",
+                slug:"Commputer-Organisation-and-Architecture",
                 credits:4
             },
             {
                 code:"CSE2632",
                 name:"Algorithm Analysis and Design 2",
+                slug:"Algorithm-Analysis-and-Design-2",
                 credits:4
             },
             {
                 code:"MTH3003",
                 name:"Applied Linear Algebra",
+                slug:"Applied-Linear-Algebra",
                 credits:4
             },
             {
                 code:"HSS2024",
                 name:"Introduction to Macroeconomics",
+                slug:"Introduction-to-Macroeconomics",
                 credits:2
             },
             {
                 code:"CHM2042",
                 name:"Introduction to Disaster Management",
+                slug:"Introduction-to-Disaster-Management",
                 credits:2
             }
         ],
@@ -133,31 +157,37 @@ const subjectsData = {
             {
                 code:"CSE3544",
                 name:"Practical Programing with C",
+                slug:"Practical-Programing-with-C",
                 credits:4
             },
             {
                 code:"CSE3249",
                 name:"Digital Principal of Operating Systems",
+                slug:"Digital-Principal-of-Operating-Systems",
                 credits:4
             },
             {
                 code:"CSE3751",
                 name:"Computer Networking: Concepts",
+                slug:"Computer-Networking-Concepts",
                 credits:4
             },
             {
                 code:"CSE3967",
                 name:"Machine Learning Concepts 1",
+                slug:"Machine-Learning-Concepts-1",
                 credits:2
             },
             {
                 code:"CSE3731",
                 name:"Introduction to Theory of Computation",
+                slug:"Introduction-to-Theory-of-Computation",
                 credits:3
             },
             {
                 code:"MTH3647",
                 name:"Funamentals of Machine Intelligence 1",
+                slug:"Funamentals-of-Machine-Intelligence-1",
                 credits:3
             }
         ],
@@ -165,31 +195,37 @@ const subjectsData = {
             {
                 code:"CSE3968",
                 name:"Machine Learning Concepts 2",
+                slug:"Machine-Learning-Concepts-2",
                 credits:2
             },
             {
                 code:"CSE3739",
                 name:"Compilers: Principles, Techniques and Tools",
+                slug:"Compilers-Principles-Techniques-and-Tools",
                 credits:3
             },
             {
                 code:"MTH3657",
                 name:"Fundamentals of Machine Intelligence 2",
+                slug:"Fundamentals-of-Machine-Intelligence-2",
                 credits:3
             },
             {
                 code:"CSE3545",
                 name:"Game Programming with C++",
+                slug:"Game-Programming-with-Cpp",
                 credits:4
             },
             {
                 code:"CSE3151",
                 name:"Introduction to Databases",
+                slug:"Introduction-to-Databases",
                 credits:4
             },
             {
                 code:"CSE3752",
                 name:"Computer Networking: Security",
+                slug:"Computer-Networking-Security",
                 credits:4
             }
         ],
@@ -197,58 +233,69 @@ const subjectsData = {
             {
                 code:"URP4201",
                 name:"Research Methodology",
+                slug:"Research-Methodology",
                 credits:2
             },
             {
                 code:"CSE4820",
                 name:"Arduino Micro controller Concepts",
+                slug:"Arduino-Micro-controller-Concepts",
                 credits:4
             },
             {
                 code:"CSE4571",
                 name:"Practical Robotics Projects with Arduino",
+                slug:"Practical-Robotics-Projects-with-Arduino",
                 credits:4
             },
             {
                 code:"CSE4571",
                 name:"Departmental Elective I",
+                slug:"Departmental-Elective-1",
                 credits:4
             },
             {
                 code:"CSE4571",
                 name:"Departmental Elective II",
+                slug:"Departmental-Elective-2",
                 credits:4
             },
             {
                 code:"CSE4571",
                 name:"Departmental Elective III",
+                slug:"Departmental-Elective-3",
                 credits:4
-            },
+            }
         ],
         8:[
             {
                 code:"",
                 name:"IKS Elective I",
+                slug:"IKS-Elective-1",
                 credits:3
             },
             {
                 code:"",
                 name:"IKS Elective II",
+                slug:"IKS-Elective-2",
                 credits:3
             },
             {
                 code:"",
                 name:"Open Elective I",
+                slug:"Open-Elective-1",
                 credits:3
             },
             {
                 code:"",
                 name:"Open Elective II",
+                slug:"Open-Elective-2",
                 credits:3
             },
             {
                 code:"URP4202",
                 name:"Final year Research Project",
+                slug:"Final-Year-Research-Project",
                 credits:4
             }
         ]
